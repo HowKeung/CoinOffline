58c1cdaed81f963d6861de518999762966f97cd6
jungel <785686918@qq.com>
2018-11-20 19:20:28 +0800

    修改为单例

--- coinoffline/src/main/java/com/jungel/coinoffline/CoinOffline.java
+++ coinoffline/src/main/java/com/jungel/coinoffline/CoinOffline.java
@@ -4,31 +4,42 @@ import android.content.Context;
 import android.text.TextUtils;
 
 import com.jungel.coinoffline.eos.eospocket.utils.Utils;
-import com.jungel.coinoffline.eth.ETHUrlHelper;
 
-import org.web3j.protocol.Web3j;
-import org.web3j.protocol.Web3jFactory;
-import org.web3j.protocol.http.HttpService;
+import org.consenlabs.tokencore.wallet.Identity;
+import org.consenlabs.tokencore.wallet.KeystoreStorage;
+import org.consenlabs.tokencore.wallet.WalletManager;
 
-public class CoinOffline {
+import java.io.File;
+
+public class CoinOffline implements KeystoreStorage {
 
     private static final String KEY_PIN_CODE = "key_pin_code";
 
     private boolean mIsShowPin = true;
     private Boolean mHasPinCode = null;
 
-    private static Context mContext;
+    private Context mContext;
+    private static CoinOffline mInstance;
 
-    public static Context getContext() {
+    public synchronized static CoinOffline getInstance() {
+        if (mInstance == null) {
+            mInstance = new CoinOffline();
+        }
+        return mInstance;
+    }
+
+    public synchronized Context getContext() {
         if (mContext == null) {
             throw new RuntimeException("must call init first");
         }
         return mContext;
     }
 
-    public static void init(Context context) {
+    public synchronized void init(Context context) {
         mContext = context;
         Utils.init(context);
+        WalletManager.storage = this;
+        WalletManager.scanWallets();
     }
 
     public synchronized boolean hasPinCode() {
@@ -53,5 +64,17 @@ public class CoinOffline {
 
     public synchronized void setShowPin(boolean showPin) {
         mIsShowPin = showPin;
+    }
+
+    public synchronized boolean hasCreateAccount() {
+        if (Identity.getCurrentIdentity() == null) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public File getKeystoreDir() {
+        return mContext.getFilesDir();
     }
 }
