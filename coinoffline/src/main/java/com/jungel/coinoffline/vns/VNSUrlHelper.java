61e0800a1e1e5277a80efe4645e36a9c4eafcddd
jungel <785686918@qq.com>
2018-11-30 12:11:53 +0800

    添加VNS记录接口地址

--- coinoffline/src/main/java/com/jungel/coinoffline/vns/VNSUrlHelper.java
+++ coinoffline/src/main/java/com/jungel/coinoffline/vns/VNSUrlHelper.java
@@ -4,4 +4,7 @@ public class VNSUrlHelper {
 
     public static final String HD_MAIN_URL = "https://192.168.1.112:12180";
     public static final String HD_TEST_URL = "http://192.168.1.112:12180";
+
+    public static final String HD_MAIN_HISTORY_URL = "http://www.vnscan" +
+            ".com/browser/transaction/findTransaction?blockNumber=-1&findType=2";
 }
