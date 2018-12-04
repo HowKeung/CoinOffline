7bdd33d0a0850c524c355dd6e41da5a6c3c14f73
jungel <785686918@qq.com>
2018-12-04 10:18:50 +0800

    替换ETH key

--- coinoffline/src/main/java/com/jungel/coinoffline/eth/ETHUrlHelper.java
+++ coinoffline/src/main/java/com/jungel/coinoffline/eth/ETHUrlHelper.java
@@ -8,13 +8,15 @@ public class ETHUrlHelper {
     public static final String PATH_HD_WALLET = "/wallet";
     public static final String KEY_WALLET_MNEMONIC = "key_wallet_mnemonic";
 
-    public static final String ETH_API_KEY = "AA4NZ17KY4WX1D84NIEAV554H555CYXQKD";
+    //    public static final String ETH_API_KEY = "AA4NZ17KY4WX1D84NIEAV554H555CYXQKD";
+    public static final String ETH_API_KEY = "HXZT2N74G67AZYU62835HE5879IYI7IP1P";
 
     public static final String HD_MAIN_HISTORY_URL = "https://api-ropsten.etherscan.io";
     public static final String HD_TEST_HISTORY_URL = "https://api-ropsten.etherscan.io";
 
     public static final String HD_ETH_HISTORY_URL =
-            "/api?module=account&action=txlist&startblock=0&endblock=99999999&sort=desc&apikey=" + ETH_API_KEY;
+            "/api?module=account&action=txlist&startblock=0&endblock=99999999&sort=desc&apikey="
+                    + ETH_API_KEY;
 
     public static final String HD_ERC_HISTORY_URL =
             "/api?module=account&action=tokentx&sort=desc&apikey=" + ETH_API_KEY;
