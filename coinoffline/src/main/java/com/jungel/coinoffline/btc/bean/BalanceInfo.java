f522c4697809425d58c093c7689ea5be289b5031
jungel <785686918@qq.com>
2018-12-13 15:48:06 +0800

    添加BTC余额属性

--- coinoffline/src/main/java/com/jungel/coinoffline/btc/bean/BalanceInfo.java
+++ coinoffline/src/main/java/com/jungel/coinoffline/btc/bean/BalanceInfo.java
@@ -1,4 +1,14 @@
 package com.jungel.coinoffline.btc.bean;
 
 public class BalanceInfo {
+
+    private double balance;
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
 }
