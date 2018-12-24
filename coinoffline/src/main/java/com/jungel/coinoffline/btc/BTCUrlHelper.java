bcb43c0842f4ac0067eed84a4c7b54cfa25a9d57
jungel <785686918@qq.com>
2018-12-24 11:38:08 +0800

    修改BTC URL

--- coinoffline/src/main/java/com/jungel/coinoffline/btc/BTCUrlHelper.java
+++ coinoffline/src/main/java/com/jungel/coinoffline/btc/BTCUrlHelper.java
@@ -5,6 +5,8 @@ public class BTCUrlHelper {
     public static final String HD_MAIN_URL = "https://mainnet-eos.token.im";
     public static final String HD_TEST_URL = "http://jungle.cryptolions.io:18888";
 
-    public static final String HD_URL_GET_BALANCE = "";
+    public static final String HD_URL_GET_BALANCE = "/q/addressbalance/";
     public static final String HD_URL_GET_RECORD = "";
+
+    public static final String HD_URL_GET_UTXO = "/unspent?active=";
 }
