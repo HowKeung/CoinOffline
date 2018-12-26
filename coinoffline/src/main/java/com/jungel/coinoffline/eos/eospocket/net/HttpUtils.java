d3d05ecf22747c1c112e6bec11adca5a2680fffc
jungel <785686918@qq.com>
2018-12-26 17:04:57 +0800

    修改请求添加header参数

--- coinoffline/src/main/java/com/jungel/coinoffline/eos/eospocket/net/HttpUtils.java
+++ coinoffline/src/main/java/com/jungel/coinoffline/eos/eospocket/net/HttpUtils.java
@@ -2,6 +2,7 @@ package com.jungel.coinoffline.eos.eospocket.net;
 
 import com.jungel.coinoffline.eos.eospocket.net.callbck.JsonCallback;
 import com.lzy.okgo.OkGo;
+import com.lzy.okgo.model.HttpHeaders;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -53,6 +54,24 @@ public class HttpUtils {
      * @param <T>      the type parameter
      * @param url      the url
      * @param tag      the tag
+     * @param map      the map
+     * @param callback the callback
+     */
+    public static <T> void postRequest(String url, Object tag, Map<String, String> map,
+                                       HttpHeaders headers, JsonCallback<T> callback) {
+        OkGo.<T>post(url)
+                .tag(tag)
+                .headers(headers)
+                .params(map)
+                .execute(callback);
+    }
+
+    /**
+     * Post request.
+     *
+     * @param <T>      the type parameter
+     * @param url      the url
+     * @param tag      the tag
      * @param parms    the parms
      * @param callback the callback
      */
@@ -60,6 +79,25 @@ public class HttpUtils {
             callback) {
         OkGo.<T>post(url)
                 .tag(tag)
+                .upJson(parms)
+                .execute(callback);
+    }
+
+    /**
+     * Post request.
+     *
+     * @param <T>      the type parameter
+     * @param url      the url
+     * @param tag      the tag
+     * @param parms    the parms
+     * @param callback the callback
+     */
+    public static <T> void postRequest(String url, Object tag, String parms, HttpHeaders headers,
+                                       JsonCallback<T>
+                                               callback) {
+        OkGo.<T>post(url)
+                .tag(tag)
+                .headers(headers)
                 .upJson(parms)
                 .execute(callback);
     }
@@ -90,10 +128,30 @@ public class HttpUtils {
      * @param json     the json
      * @param callback the callback
      */
-    public static <T> void postRequest(String url, Object tag, JSONArray json, JsonCallback<T>
-            callback) {
+    public static <T> void postRequest(String url, Object tag, JSONObject json, HttpHeaders
+            headers, JsonCallback<T>
+                                               callback) {
         OkGo.<T>post(url)
                 .tag(tag)
+                .headers(headers)
+                .upJson(json)
+                .execute(callback);
+    }
+
+    /**
+     * Post request.
+     *
+     * @param <T>      the type parameter
+     * @param url      the url
+     * @param tag      the tag
+     * @param json     the json
+     * @param callback the callback
+     */
+    public static <T> void postRequest(String url, Object tag, JSONArray json, HttpHeaders
+            headers, JsonCallback<T> callback) {
+        OkGo.<T>post(url)
+                .tag(tag)
+                .headers(headers)
                 .upJson(json)
                 .execute(callback);
     }
